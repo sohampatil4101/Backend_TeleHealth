96c87146bdb4dbdba98264321dda879f825647c0
Soham Patil <patilsoham390@gmail.com>
2024-06-15 13:12:53 +0530

    made user changes

--- routes/doctor.js
+++ routes/doctor.js
@@ -158,6 +158,17 @@ router.get('/fetchallappoinments', fetchdoctor, async(req, res) =>{
     res.status(500).send("Some error occured")
     }
 })
+router.get('/acceptreject', fetchdoctor, async(req, res) =>{
+    try {
+        const notes = await appoinment.findOne({_id: req.body.id});
+        notes.confirm = req.body.mssg
+        notes.save()
+        res.json(notes)
+    } catch (error) {
+    console.log(error.message)
+    res.status(500).send("Some error occured")
+    }
+})
 
 
 
